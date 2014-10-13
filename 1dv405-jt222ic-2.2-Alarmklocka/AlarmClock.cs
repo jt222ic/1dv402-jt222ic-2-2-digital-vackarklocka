204d4db19dd2ede343e1d6b018d468c86cffe86b
jt222ic <jt222ic@student.lnu.se>
2014-10-13 18:40:55 +0200

    1st commit  avklarad test 1,2,3 TIck tock problem = att loopen ska ske, dock bara 1 värde som kom

---
+++ 1dv405-jt222ic-2.2-Alarmklocka/AlarmClock.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1dv405_jt222ic_2._2_Alarmklocka
+{
+    class AlarmClock
+    {              //// fält ////
+        private int _alarmHour;                /// klasser indelad i egenskaper
+        private int _alarmMinute;            //// underline för att förtydliga att 
+        private int _hour;                   ////  variabeln tillhör private klass.
+        private int _minute;
+
+
+
+        public int AlarmHour        /// klass gör att man använder sig av private fält och göra dem till publika
+        {
+            get { return _alarmHour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException("Alarmtimmen ligger inte i intervallet 0-23 ");
+                }
+                _alarmHour = value;
+            }
+        }
+        public int AlarmMinute
+        {
+            get { return _alarmMinute; }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException("Alarmminuten ligger inte i intervallet 0-59");
+                }
+                _alarmMinute = value;
+            }
+
+
+        }
+        public int Hour
+        {
+            get { return _hour; }
+            set
+            {
+                if (value < 0 || value > 23)
+                {
+                    throw new ArgumentException("timmer ligger inte i intervallet 0-23");
+                }
+
+                _hour = value;
+            }
+
+
+        }
+        public int Minute
+        {
+            get { return _minute; }
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException("minuten ligger inte i intervallet 0-59");
+                }
+                _minute = value;
+
+
+
+            }
+
+        }
+
+
+        ///kontruktorer samlas sig värden från egenskaper ////
+
+        public AlarmClock()
+            : this(0,0)   /// körs två parameter
+        {
+            /// tom eftersom uppgiften säger det. DEt ska inte upprepas med sig själv
+        }
+
+
+        public AlarmClock(int hour, int minute)
+            : this(hour, minute,0,0)  /// körs för 4 parameter
+        {
+            ///tom kropp         
+        }
+
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+            AlarmHour = alarmHour;   /// public klasser hämtar värden/egenskaper från kontruktor 
+            AlarmMinute = alarmMinute;
+            Hour = hour;
+            Minute = minute;
+
+            
+                                             ///hämtar in världen från egenskaper
+        }
+        // metoden ansvarar om minuternas värden ökar med 1 //
+        // övergår till True om värden överstämmer sig med Alarmtiden//
+        public bool TickTock()
+        {
+                                    ///
+            if (Minute < 59)       /// lopar tiden det ska gå 13 gånger.
+            {
+                ++Minute;       // om minuten är mindre än 59 plussas 1 
+            }
+            else
+            {
+                Minute = 0;        
+                if (Hour < 23)
+                {
+                    ++Hour;
+                    
+                }
+                else
+                {
+                    Hour = 0;
+                }                   // om minuten är över 59 blir värdet 0 igen
+            }                       // digital klockan har inte 24 i visning alltså 0 igen
+            return AlarmHour == Hour && AlarmMinute == Minute;  //  returnera världet
+
+
+        }
+        public override string ToString()      ///// override över basmetoden  klass object som heter redan tostring
+        {                                      //// detta är en Metod // blanda det inte med egenskaper 
+
+            StringBuilder Display = new StringBuilder();
+
+
+            Display.AppendFormat("{0,5}:0{1} <{2}:0{3}>", Hour, Minute, AlarmHour, AlarmMinute);
+
+            if (Minute >= 10)
+            {
+                Display.Remove(6, 1);
+            }
+                                                 
+            if (AlarmMinute >= 10)            /// lite osäker hur det fungerar
+            {
+                Display.Remove(12, 1);
+            }
+
+            return Display.ToString();
+
+            /// alla konstruktor har samma namn som klassen// 
+            /// metodens roll att returna en sträng som representeras värdet av en instans av klassen AlarmClock
+        }
+    }
+}
---
+++ 1dv405-jt222ic-2.2-Alarmklocka/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1dv405_jt222ic_2._2_Alarmklocka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// test av standard konstruktorn test 1 ///
+            AlarmClock test = new AlarmClock();
+            ViewTestHeader("test 1.\nTest av standard konstruktorn");                                        //// test är objektet
+            Console.WriteLine(test.ToString());
+            // test 2 /// 2 parameter ////
+            AlarmClock test2 = new AlarmClock(9, 42);
+            ViewTestHeader("test 2.\nTest av standard konstruktorn");
+            Console.WriteLine(test2.ToString());
+
+
+            ////test 3 //// 4 parameter ///
+            AlarmClock test3 = new AlarmClock(13, 24, 7, 23);
+            ViewTestHeader("test 3. \nTest av standard konstruktorn");
+            Console.WriteLine(test3.ToString());
+
+            // 3 konstruktorer 2 metoder 1 fält med alla variablar för att få fram 3 första metoden//
+            // omg så jobbig........ 
+        
+             //början av test 4 //
+            AlarmClock test4 = new AlarmClock(23, 58, 7, 5);
+            ViewTestHeader("Test 4: test utav TickTock Metoden så att klockan går 13 minuter.\n");
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ╔══════════════════════════════════════════════════════════════╗ ");
+            Console.WriteLine(" ║ Väckarklocka, en vacker uppfinning som alla hatar på måndag. ║ ");
+            Console.WriteLine(" ╚══════════════════════════════════════════════════════════════╝ ");
+            Console.ResetColor();
+            Run(test4,13);                     // visar att jag körs 13 gånger
+
+            AlarmClock test5 = new AlarmClock(6, 12, 6, 15);
+            ViewTestHeader("Test 5: test slå av alarmet\n");
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ╔══════════════════════════════════════════════════════════════╗ ");
+            Console.WriteLine(" ║ Väckarklocka, en vacker uppfinning som alla hatar på måndag. ║ ");
+            Console.WriteLine(" ╚══════════════════════════════════════════════════════════════╝ ");
+            Console.ResetColor();
+            Run(test5,6);
+        }
+        static void Run(AlarmClock test45, int minutes)   // metoden för TICK-TOCK
+        {                                                 //döptes test 45 hämtas från test 4 och 5
+            if (test45.TickTock())
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("♫");
+                Console.WriteLine(test45.ToString() + "Tjut tjut tjut!");
+            }
+            else
+            {
+                Console.ResetColor();
+                Console.Write(" ");
+                Console.WriteLine(test45.ToString());
+            }
+
+        }
+
+
+        private static void ViewTestHeader(string header)      /// 2 metoder för att kunna skriva fram test 1,2 ,3
+        {                                                      /// string metoden och override metoden
+            Console.WriteLine();
+            /// metod för att skriva de 3 första testerna
+            Console.WriteLine(header);
+
+        }
+    }
+}
