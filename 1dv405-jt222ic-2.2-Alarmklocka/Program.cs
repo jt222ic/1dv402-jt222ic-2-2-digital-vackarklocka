18c239c9b3d9bc2b95236e4d3801c8e22809cdd7
jt222ic <jt222ic@student.lnu.se>
2014-10-14 16:14:28 +0200

    2nd push avklarad test 5,6,7 tester och metoder inlagd

--- 1dv405-jt222ic-2.2-Alarmklocka/Program.cs
+++ 1dv405-jt222ic-2.2-Alarmklocka/Program.cs
@@ -34,40 +34,111 @@ namespace _1dv405_jt222ic_2._2_Alarmklocka
             Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(" ╔══════════════════════════════════════════════════════════════╗ ");
-            Console.WriteLine(" ║ Väckarklocka, en vacker uppfinning som alla hatar på måndag. ║ ");
+            Console.WriteLine(" ║ Väckarklocka, 13 gånger                                      ║ ");
             Console.WriteLine(" ╚══════════════════════════════════════════════════════════════╝ ");
             Console.ResetColor();
             Run(test4,13);                     // visar att jag körs 13 gånger
 
+
+            //test 5//
             AlarmClock test5 = new AlarmClock(6, 12, 6, 15);
             ViewTestHeader("Test 5: test slå av alarmet\n");
             Console.BackgroundColor = ConsoleColor.DarkGreen;
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(" ╔══════════════════════════════════════════════════════════════╗ ");
-            Console.WriteLine(" ║ Väckarklocka, en vacker uppfinning som alla hatar på måndag. ║ ");
+            Console.WriteLine(" ║ Väckarklocka, 6gånger                                        ║ ");
             Console.WriteLine(" ╚══════════════════════════════════════════════════════════════╝ ");
             Console.ResetColor();
             Run(test5,6);
+
+            //test 6 //
+            
+            AlarmClock test6 = new AlarmClock();
+            ViewTestHeader("Test 6: Test så att man inte kan ange för höga variabler.\n");
+
+
+            try
+            {
+                test6.Hour = 45;
+            }
+            catch (ArgumentException failure)
+            {
+                ViewErrorMessage(failure.Message);
+            }
+
+            try
+            {
+                test6.Minute = 70;
+            }
+            catch (ArgumentException failure)
+            {
+                ViewErrorMessage(failure.Message);
+            }
+
+            try
+            {
+                test6.AlarmHour = 42;
+            }
+            catch (ArgumentException failure)
+            {
+                ViewErrorMessage(failure.Message);
+            }
+            try
+            {
+                test6.AlarmMinute = 70;
+            }
+            catch (ArgumentException failure)
+            {
+                ViewErrorMessage(failure.Message);
+            }
+             ViewTestHeader("Test 7: Test som kollar så att när tid/ alarmtid blir tilldelade felaktiga värden.\n");
+            try
+            {
+                AlarmClock test7 = new AlarmClock(42, 0);
+            }
+            catch (ArgumentException failure)
+            {
+                ViewErrorMessage(failure.Message);
+            }
+            try
+            {
+                AlarmClock test7 = new AlarmClock(0, 0, 42, 0);
+            }
+            catch (ArgumentException failure)
+            {
+                ViewErrorMessage(failure.Message);
+            }
+        
+
         }
         static void Run(AlarmClock test45, int minutes)   // metoden för TICK-TOCK
-        {                                                 //döptes test 45 hämtas från test 4 och 5
-            if (test45.TickTock())
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.Write("♫");
-                Console.WriteLine(test45.ToString() + "Tjut tjut tjut!");
-            }
-            else
-            {
-                Console.ResetColor();
-                Console.Write(" ");
-                Console.WriteLine(test45.ToString());
+        {
+            for (int i = 0; minutes > i; ++i)
+            {                         //döptes test 45 hämtas från test 4 och 5
+                if (test45.TickTock())
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("♫");
+                    Console.WriteLine(test45.ToString() + "Tjut tjut tjut!");
+                }
+                else
+                {
+                    Console.ResetColor();
+                    Console.Write(" ");
+                    Console.WriteLine(test45.ToString());
+                }
+
             }
 
         }
 
-
+        private static void ViewErrorMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
         private static void ViewTestHeader(string header)      /// 2 metoder för att kunna skriva fram test 1,2 ,3
         {                                                      /// string metoden och override metoden
             Console.WriteLine();
